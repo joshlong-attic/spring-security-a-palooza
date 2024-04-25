b82c74f6ed3bfe49b846533378f719e435f816c1
Josh Long <josh@joshlong.com>
2024-04-25 15:08:48 -0700

    oauth\ ftw

--- oauth/auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ oauth/auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -24,7 +24,6 @@ public class AuthServiceApplication {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -39,7 +38,7 @@ public class AuthServiceApplication {
                 .clientName(browser)
                 .clientSecret("{bcrypt}$2a$10$us1QCGw8FILSFp7jo5HCMuHoLKvUFV61PVwsEEzQNekS3EiIT9ND2") // 'pw' (without quotes)
                 .authorizationGrantTypes(a -> a.addAll(Set.of(AuthorizationGrantType.AUTHORIZATION_CODE, AuthorizationGrantType.REFRESH_TOKEN)))
-                .redirectUri("http://127.0.0.1:1010/login/oauth2/code/spring")
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/browser")
                 .scope("openid")
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
@@ -51,7 +50,7 @@ public class AuthServiceApplication {
     UserDetailsService users() {
         var user = User
                 .withUsername("cora")
-                .password("{bcrypt}$2a$10$E8FEFmBZNKMBTZqGw1LRVuTujXeRGCCxtg.ui6DVZSLhsV1GFYm1u")
+                .password("{bcrypt}$2a$10$E8FEFmBZNKMBTZqGw1LRVuTujXeRGCCxtg.ui6DVZSLhsV1GFYm1u")// 'pw' (without quotes)
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
