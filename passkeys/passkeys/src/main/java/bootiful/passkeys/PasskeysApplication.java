a0e1a10e344575eec7ecdb3e8dc4c3108e966634
Josh Long <josh@joshlong.com>
2024-04-25 13:50:47 -0700

    first cut.

---
+++ passkeys/passkeys/src/main/java/bootiful/passkeys/PasskeysApplication.java
@@ -0,0 +1,51 @@
+package bootiful.passkeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.WebauthnConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+
+
+@SpringBootApplication
+public class PasskeysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasskeysApplication.class, args);
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        var builder = User.withDefaultPasswordEncoder().roles("USER").password("pw");
+        return new InMemoryUserDetailsManager(
+                builder.username("jlong").build(),
+                builder.username("ciberkleid").build()
+        );
+    }
+
+    @Bean
+    DefaultSecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(http -> http
+                        .requestMatchers("/login/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .with(new WebauthnConfigurer<>(), passkeys ->
+                        passkeys.allowedOrigins("http://localhost:8080")
+                                .rpId("localhost")
+                                .rpName("Bootiful WebAuthn")
+                )
+                .build();
+
+    }
+
+}
+
